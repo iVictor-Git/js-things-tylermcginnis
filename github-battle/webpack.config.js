43da021eac84fe398331a9b2ebdc5f57defcfc9a
iVictor-Git <Victor.n.tran.social@outlook.com>
2018-09-25 22:36:42 -0400

    adding public path prop to output object, and devServer prop to config

--- github-battle/webpack.config.js
+++ github-battle/webpack.config.js
@@ -6,12 +6,16 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'), // root + /dist/ +
     filename: 'index_bundle.js', // index_bundle.js => github-battle/dist/index_bundle.js
+    publicPath: '/',
   },
   module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
     ],
+  },
+  devServer: {
+    historyApiFallback: true,
   },
   mode: 'development',
   plugins: [
