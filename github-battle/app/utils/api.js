805a3f03e8e20e53d834f0d659d93077b345b6ea
Victor <victor.n.tran@outlook.com>
2018-09-27 15:49:44 -0400

    last destructure of fetchPopularRepos

--- github-battle/app/utils/api.js
+++ github-battle/app/utils/api.js
@@ -42,6 +42,6 @@ module.exports = {
     const encodedURI = window.encodeURI(
       `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`,
     );
-    return axios.get(encodedURI).then(response => response.data.items);
+    return axios.get(encodedURI).then(({ data: { items } }) => items);
   },
 };
