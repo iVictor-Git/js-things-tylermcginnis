23505e30a8da8a3f96e20a08478e6af196a86079
Victor <victor.n.tran@outlook.com>
2018-09-27 20:07:56 -0400

    fix fetchPopularRepos

--- github-battle/app/utils/api.js
+++ github-battle/app/utils/api.js
@@ -37,7 +37,7 @@ const battle = players =>
     .then(sortPlayers)
     .catch(handleError);
 
-const fetchPopularRepos = languages => {
+const fetchPopularRepos = language => {
   const encodedURI = window.encodeURI(
     `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`,
   );
