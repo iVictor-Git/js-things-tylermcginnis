140d67116bed5e432b19007826b074e2eac4ea57
Victor <victor.n.tran@outlook.com>
2018-09-29 09:07:35 -0400

    wrap function in brackets and invoke async functions

--- goals-todos/src/actions/shared.js
+++ goals-todos/src/actions/shared.js
@@ -4,7 +4,8 @@ export const RECEIVE_DATA = 'RECEIVE_DATA';
 
 const receiveData = (todos, goals) => ({ type: RECEIVE_DATA, todos, goals });
 
-export const handleInitialData = () => dispatch =>
-  Promise.all([fetchTodos, fetchGoals]).then(([todos, goals]) =>
-    dispatch(receiveData(todos, goals)),
-  );
+export const handleInitialData = () => dispatch => {
+  Promise.all([fetchTodos(), fetchGoals()]).then(([todos, goals]) => {
+    dispatch(receiveData(todos, goals));
+  });
+};
